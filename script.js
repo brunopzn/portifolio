8a2a41846733b8fd7e3256b78c1a25b49dd4a2be
Brunopzn <brunaohp@gmail.com>
2025-07-24 17:08:37 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,61 @@
+
+/* codigo para carregamento da barra skill html */
+document.addEventListener('DOMContentLoaded', () => {
+    const barra = document.querySelector('.barhtml');
+    if (barra) {
+        barra.classList.add('animar');
+       
+    }
+})
+/* fim html */
+
+
+/* codigo para carregamento da barra skill CSS */
+document.addEventListener('DOMContentLoaded', () => {
+    const barra = document.querySelector('.barcss');
+    if (barra) {
+        barra.classList.add('animar');
+       
+    }
+})
+/* fim CSS */
+
+/* codigo para carregamento da barra skill javascript */
+document.addEventListener('DOMContentLoaded', () => {
+    const barra = document.querySelector('.barjs');
+    if (barra) {
+        barra.classList.add('animar');
+       
+    }
+})
+/* fim pyrhon*/
+
+/* codigo para carregamento da barra skill CSS */
+document.addEventListener('DOMContentLoaded', () => {
+    const barra = document.querySelector('.barpy');
+    if (barra) {
+        barra.classList.add('animar');
+       
+    }
+})
+/* fim python */
+
+/* codigo para carregamento da barra skill sql */
+document.addEventListener('DOMContentLoaded', () => {
+    const barra = document.querySelector('.barsql');
+    if (barra) {
+        barra.classList.add('animar');
+       
+    }
+})
+/* fim sql */
+
+/* codigo para carregamento da barra skill react */
+document.addEventListener('DOMContentLoaded', () => {
+    const barra = document.querySelector('.barreact');
+    if (barra) {
+        barra.classList.add('animar');
+       
+    }
+})
+/* fim react */
